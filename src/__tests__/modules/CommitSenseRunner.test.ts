eb3abd118abdbe5f4b1d8e30ef94afe84d958bda
ericthecurious <hello@ericthecurious.com>
2025-10-04 23:02:44 -0600

    patch: creates GitAutocloner

--- src/__tests__/modules/CommitSenseRunner.test.ts
+++ src/__tests__/modules/CommitSenseRunner.test.ts
@@ -1,4 +1,5 @@
 import AbstractSpruceTest, { test, assert } from '@sprucelabs/test-utils'
+import { FakeAutocloner, GitAutocloner } from '@neurodevs/meta-node'
 import CommitSenseRunner, { CommitSense } from '../../modules/CommitSenseRunner'
 
 export default class CommitSenseRunnerTest extends AbstractSpruceTest {
@@ -7,12 +8,28 @@ export default class CommitSenseRunnerTest extends AbstractSpruceTest {
     protected static async beforeEach() {
         await super.beforeEach()
 
+        this.setFakeAutocloner()
+
         this.instance = this.CommitSenseRunner()
     }
 
     @test()
     protected static async createsInstance() {
         assert.isTruthy(this.instance, 'Failed to create instance!')
+    }
+
+    @test()
+    protected static async createsGitAutocloner() {
+        assert.isEqual(
+            FakeAutocloner.numCallsToConstructor,
+            1,
+            'Did not create GitAutocloner!'
+        )
+    }
+
+    private static setFakeAutocloner() {
+        GitAutocloner.Class = FakeAutocloner
+        FakeAutocloner.resetTestDouble()
     }
 
     private static CommitSenseRunner() {
--- src/modules/CommitSenseRunner.ts
+++ src/modules/CommitSenseRunner.ts
@@ -1,10 +1,17 @@
+import { GitAutocloner } from '@neurodevs/meta-node'
+
 export default class CommitSenseRunner implements CommitSense {
     public static Class?: CommitSenseConstructor
 
     protected constructor() {}
 
     public static Create() {
+        this.GitAutocloner()
         return new (this.Class ?? this)()
+    }
+
+    private static GitAutocloner() {
+        GitAutocloner.Create()
     }
 }
 
