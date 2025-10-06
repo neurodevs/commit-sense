eb12bac41c01e9cad54c8442e6cd7441c656c07d
ericthecurious <hello@ericthecurious.com>
2025-10-06 03:52:39 -0600

    minor: create FakeCommitSense

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
-//exports go here
+// CommitSense
+
+export { default as CommitSenseRunner } from './modules/CommitSenseRunner'
+export * from './modules/CommitSenseRunner'
+
+export { default as FakeCommitSense } from './testDoubles/CommitSense/FakeCommitSense'
+export * from './testDoubles/CommitSense/FakeCommitSense'
+
+// vscode
+
+export { default as fakeVscode } from './testDoubles/vscode/fakeVscode'
+export * from './testDoubles/vscode/fakeVscode'
+
+export { default as FakeWorkspace } from './testDoubles/vscode/FakeWorkspace'
+export * from './testDoubles/vscode/FakeWorkspace'
---
+++ src/testDoubles/CommitSense/FakeCommitSense.ts
@@ -0,0 +1,32 @@
+import {
+    CommitSense,
+    CommitSenseConstructorOptions,
+} from '../../modules/CommitSenseRunner'
+
+export default class FakeCommitSense implements CommitSense {
+    public static callsToConstructor: (
+        | CommitSenseConstructorOptions
+        | undefined
+    )[] = []
+
+    public static numCallsToInitialize = 0
+    public static numCallsToStart = 0
+
+    public constructor(options?: CommitSenseConstructorOptions) {
+        FakeCommitSense.callsToConstructor.push(options)
+    }
+
+    public async initialize() {
+        FakeCommitSense.numCallsToInitialize++
+    }
+
+    public async start() {
+        FakeCommitSense.numCallsToStart++
+    }
+
+    public static resetTestDouble() {
+        this.callsToConstructor = []
+        this.numCallsToInitialize = 0
+        this.numCallsToStart = 0
+    }
+}
